9be62e85ce55274831c939fe7010ed49d577e167
florianGierlichs <gierlichs@gmx.de>
2020-06-05 17:14:55 +0200

    Remove unnecessary code

--- client/src/components/Desktop/Email/MailHeader.js
+++ client/src/components/Desktop/Email/MailHeader.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-// import Senden from '../../../assets/mail-icons/senden.svg';
-// import Gesendet from '../../../assets/mail-icons/gesendet.svg';
-// import Gelesen from '../../../assets/mail-icons/gelesen.svg';
-// import Ungelesen from '../../../assets/mail-icons/ungelesen.svg';
-// import SendenIcon from './MailNavigationIcon';
 import MailNavigation from './MailNavigation';
-
-// #2C35BB
 
 const Container = styled.div`
   background-color: #2c35bb;
@@ -24,61 +17,11 @@ const TitleContainer = styled.div`
   font-weight: bold;
 `;
 
-// const NavButton = styled.button`
-//   display: flex;
-//   align-items: center;
-//   background-color: #2c35bb;
-//   border: none;
-//   color: white;
-//   font-size: 24px;
-//   font-weight: bold;
-//   &:focus {
-//     color: #2c35bb;
-//     background-color: white;
-//   }
-// `;
-
-// const IconContainer = styled.div`
-//   width: 50px;
-//   display: flex;
-//   margin-right: 10px;
-//   margin-left: 35px;
-// `;
-
-// const Icon = styled.img`
-//   width: 100%;
-//   height: 100%;
-// `;
-
 export default function MailHeader() {
   return (
     <Container>
       <TitleContainer>GE-Mail</TitleContainer>
-      {/* <NavButton>
-        <IconContainer>
-          <SendenIcon active={active} />
-        </IconContainer>
-        senden
-      </NavButton> */}
-      <MailNavigation label="senden" />
-      {/* <NavButton>
-        <IconContainer>
-          <Icon src={Gesendet} />
-        </IconContainer>
-        gesendet
-      </NavButton>
-      <NavButton>
-        <IconContainer>
-          <Icon src={Gelesen} />
-        </IconContainer>
-        gelesen
-      </NavButton>
-      <NavButton>
-        <IconContainer>
-          <Icon src={Ungelesen} />
-        </IconContainer>
-        ungelesen
-      </NavButton> */}
+      <MailNavigation />
     </Container>
   );
 }
