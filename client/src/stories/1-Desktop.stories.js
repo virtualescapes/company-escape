55650968fd1aaf930f602da8e0f5ceabd914a45e
phlgr <philippm.gartz@gmail.com>
2020-06-17 12:16:13 +0200

    Update Story Name

---
+++ client/src/stories/1-Desktop.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Desktop from '../components/Desktop/Desktop';
+
+export default {
+  title: 'Desktop',
+  component: Desktop,
+};
+
+export const Standard = () => <Desktop />;
