a85dd6230ec582656cb592b04e51f4ca54633756
Susanne1307 <susanneholocher@mail.de>
2020-06-17 13:07:39 +0200

    Create first social media example

---
+++ client/src/stories/Finstagram.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Insta from '../components/Desktop/social-media/Finstagram';
+
+export default {
+  title: 'Insta',
+  component: Insta,
+};
+
+export const InstaPage = () => <Insta />;
