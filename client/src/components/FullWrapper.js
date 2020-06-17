80eb2b46ee0a52580a07330c89bd57f78c3803ff
phlgr <philippm.gartz@gmail.com>
2020-06-17 10:24:54 +0200

    Remove Padding from FullWrapper

--- client/src/components/FullWrapper.js
+++ client/src/components/FullWrapper.js
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 
 const FullWrapper = styled.div`
   position: fixed;
-  padding: 20px;
   height: 100%;
   width: 100vw;
   background: ${(props) => (props.background ? props.background : 'white')};
