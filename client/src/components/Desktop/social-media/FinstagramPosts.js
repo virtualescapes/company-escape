b44eb60c3eb89debc77d1bf873602865a0713468
Susanne1307 <susanneholocher@mail.de>
2020-06-18 12:52:09 +0200

    Add posts section

---
+++ client/src/components/Desktop/social-media/FinstagramPosts.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropType from 'prop-types';
+
+const PostSection = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  width: 70%;
+  margin-bottom: 10px;
+  padding: 0px;
+`;
+
+const Post = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: calc(33.33% - 20px);
+  margin: 20px 10px 0px 10px;
+`;
+
+const PostImage = styled.img`
+  width: 100%;
+`;
+
+const Test = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+function FinstagramPosts({ posts }) {
+  return (
+    <Test>
+      <PostSection>
+        {posts.map((post) => {
+          return (
+            <Post key={post}>
+              <PostImage src={post} />
+            </Post>
+          );
+        })}
+      </PostSection>
+    </Test>
+  );
+}
+
+export default FinstagramPosts;
+
+FinstagramPosts.propTypes = {
+  posts: PropType.array,
+};
--- client/src/stories/Finstagram.stories.js
+++ client/src/stories/Finstagram.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Insta from '../components/Desktop/social-media/Finstagram';
+import Finstagram from '../components/Desktop/social-media/Finstagram';
+import Piece1 from '../assets/puzzle-pieces/1.png';
+import Piece2 from '../assets/puzzle-pieces/2.png';
+import Piece3 from '../assets/puzzle-pieces/3.png';
+import Piece4 from '../assets/puzzle-pieces/4.png';
+import Sarah from '../assets/social-media/profile.png';
 
 export default {
   title: 'Insta',
-  component: Insta,
+  component: Finstagram,
 };
 
-export const InstaPage = () => <Insta />;
+const posts = [Piece1, Piece2, Piece3, Piece4, Sarah];
+
+export const InstaSarah = () => <Finstagram posts={posts} />;
