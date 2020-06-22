4a9772a91eac71d70a0093aa5e63ec6ec0276437
SebastianStupp <s.stupp@outlook.de>
2020-06-22 11:29:08 +0200

    Add icon area to header and edit storybook

--- client/src/stories/Finstagram.stories.js
+++ client/src/stories/Finstagram.stories.js
@@ -5,21 +5,22 @@ import Piece2 from '../assets/puzzle-pieces/2.png';
 import Piece3 from '../assets/puzzle-pieces/3.png';
 import Piece4 from '../assets/puzzle-pieces/4.png';
 import Sarah from '../assets/social-media/profile.png';
-import Sebastian from '../assets/social-media/sebastian.jpg';
 
 export default {
-  title: 'Insta',
+  title: 'Finsta',
   component: Finstagram,
 };
 
 const user = {
-  name: 'Sebastian',
-  image: Sebastian,
+  name: 'Sarah',
+  image: Sarah,
   posts: '2',
   followers: '123',
   following: '24',
+  about:
+    'Hallo ich bin die wilde Sarah und ich bin offen für neue Abenteuer 👉🏿 👌 💦',
 };
 
 const posts = [Piece1, Piece2, Piece3, Piece4, Sarah];
 
-export const InstaSebastian = () => <Finstagram posts={posts} user={user} />;
+export const FinstaSarah = () => <Finstagram posts={posts} user={user} />;
