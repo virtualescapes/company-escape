9b8743ad34d2ab7dc3110588954cc29abbfd4cc2
florianGierlichs <gierlichs@gmx.de>
2020-06-22 17:55:41 +0200

    Delete unnecessary stories, fix typo

--- client/src/stories/FinstagramModal.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import FinstagramModal from '../components/Desktop/social-media/FinstagramModal';
-
-export default {
-  title: 'Modal',
-  component: FinstagramModal,
-};
-
-export const Modal = () => <FinstagramModal />;
