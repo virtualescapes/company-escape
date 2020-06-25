22fe5a96595036162272d8145e000a137de4c0e9
Susanne1307 <susanneholocher@mail.de>
2020-06-25 13:13:56 +0200

    Clean up post modal files

--- client/src/stories/FinstagramModal.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import FinstagramPostModal from '../components/Desktop/social-media/FinstagramPostModal';
-
-export default {
-  title: 'Modal',
-  component: FinstagramPostModal,
-};
-
-export const Modal = () => <FinstagramPostModal />;
