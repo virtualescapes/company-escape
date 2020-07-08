d70579632c94282722486e6c49b5472bdd4d1c01
Susanne1307 <susanneholocher@mail.de>
2020-07-08 17:29:38 +0200

    Add navigation

---
+++ client/src/components/utils/PageNavigationHover.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const PageNavigationHover = styled.div`
+  position: absolute;
+
+  &:hover {
+    background: rgba(220, 220, 220, 0.4);
+  }
+`;
+
+export default PageNavigationHover;
