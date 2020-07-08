c426a10d9a25c5bdc5c3bbaaf6553d44cbed9f7f
Susanne1307 <susanneholocher@mail.de>
2020-07-08 17:30:19 +0200

    Add pages

---
+++ client/src/pages/Laptop.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import DesktopLogin from '../components/Desktop/DesktopLogin';
+
+export default function Laptop() {
+  return <DesktopLogin />;
+}
---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Welcome() {
+  return <div>Hallo :)</div>;
+}
